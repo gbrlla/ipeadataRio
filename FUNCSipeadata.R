91bde620c26bedc9ff82a2e5b2b137fc46ddc0ac
gomesleduardo <gomes.leduardo@gmail.com>
2018-08-15 19:27:53 -0300

    Entrada do source - 15/08/2018

---
+++ FUNCSipeadata.R
@@ -0,0 +1,14 @@
+# CONFAZ12 --------------------------------------
+source("//Srjn3/area_corporativa/Projeto_IPEADATA/Geral/ipeadataRio/Funcoes/CONFAZ12WB.R")
+
+# Verificacao GENERICA --------------------------------------
+source("//Srjn3/area_corporativa/Projeto_IPEADATA/Geral/ipeadataRio/Funcoes/GENERICAverif.R")
+
+# MLD --------------------------------------
+source("//Srjn3/area_corporativa/Projeto_IPEADATA/Geral/ipeadataRio/Funcoes/MLD.R")
+
+# SECEX12 - Importacao e Exportacao --------------------------------------
+source("//Srjn3/area_corporativa/Projeto_IPEADATA/Geral/ipeadataRio/Funcoes/SECEX12FOB12.R")
+
+# UTEIS - Encontra serie, situavar e descontinuidade --------------------------------------
+source("//Srjn3/area_corporativa/Projeto_IPEADATA/Geral/ipeadataRio/Funcoes/UTEIStroll.R")
