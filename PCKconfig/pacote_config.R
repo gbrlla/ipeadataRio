a78c20c3114a0f44fa6e630b1df11b4683b41605
gomesleduardo <gomes.leduardo@gmail.com>
2018-08-17 15:47:37 -0300

    Atualização 2 - 17/08/2018

--- PCKconfig/pacote_config.R
+++ PCKconfig/pacote_config.R
@@ -24,31 +24,32 @@ rm(i,pacotes)
 # CRIANDO PACOTE ----------------------------------------
 
 #------ 1. Criando pacote direcionando a pasta compartilhada pelo Git
-devtools::setup("D:/Users/E207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
+devtools::setup("C:/Users/b207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
 
 #------ 2. Documentando
-devtools::document(pkg = "D:/Users/E207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
+devtools::document(pkg = "C:/Users/b207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
 
 #------ 2.1 Documentando 2
-roxygen2::roxygenise(package.dir = "D:/Users/E207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
+roxygen2::roxygenise(package.dir = "C:/Users/b207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
 
 #------ 3 Adicionando licenca
-devtools::use_mit_license(pkg = "D:/Users/E207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
+devtools::use_mit_license(pkg = "C:/Users/b207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
 
 #------ 4 Adicionando dados
-users <- read.csv2("D:/Users/E207056565/Desktop/Ipea/ipeadataRio/PCKconfig/DB/users.csv")
-devtools::use_data(users,pkg = "D:/Users/E207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
+users <- read.csv2("C:/Users/b207056565/Desktop/Ipea/ipeadataRio/PCKconfig/DB/users.csv")
+devtools::use_data(users,pkg = "C:/Users/b207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
 # devtools::use_data(users,internal = TRUE)
 
-codpaisesSECEX12FOB12 <- read.csv2("D:/Users/E207056565/Desktop/Ipea/ipeadataRio/PCKconfig/DB/codpaisesSECEX12FOB12.csv")
-devtools::use_data(codpaisesSECEX12FOB12,pkg = "D:/Users/E207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
+codpaisesSECEX12FOB12 <- read.csv2("C:/Users/b207056565/Desktop/Ipea/ipeadataRio/PCKconfig/DB/codpaisesSECEX12FOB12.csv")
+devtools::use_data(codpaisesSECEX12FOB12,pkg = "C:/Users/b207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
 
-codterritCONFAZ12 <- read.csv2("D:/Users/E207056565/Desktop/Ipea/ipeadataRio/PCKconfig/DB/codterritCONFAZ12.csv")
-devtools::use_data(codterritCONFAZ12,pkg = "D:/Users/E207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
+codterritCONFAZ12 <- read.csv2("C:/Users/b207056565/Desktop/Ipea/ipeadataRio/PCKconfig/DB/codterritCONFAZ12.csv")
+devtools::use_data(codterritCONFAZ12,pkg = "C:/Users/b207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
 
 #------ 5 Checando pacote
-devtools::check(pkg = "D:/Users/E207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
+devtools::check(pkg = "C:/Users/b207056565/Desktop/Ipea/ipeadataRio/ipeadataRio")
 
 # Use sempre devtools::check() para checar se seu pacote está 100% bem construído.
 # Use devtools::use_package() para usar funções de outros pacotes.
 # Use devtools::use_data() para adicionar dados ao seu pacote.
+
