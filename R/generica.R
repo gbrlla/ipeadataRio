d1610686f1e7de93695ddb4d93426b44e7add088
gomesleduardo <gomes.leduardo@gmail.com>
2018-11-29 14:41:36 -0200

    Recodificação - 29/11/2018

--- R/generica.R
+++ R/generica.R
@@ -14,29 +14,32 @@
 #' @description Retorna a planilha \code{GENERICA} para verificacao a
 #' partir do \code{SERCODIGOTROLL}.
 #'
-#' @param serie Vetor contendo o \code{SERCODIGOTROLL} da(s) serie(s) requisitada(s).
+#' @param serie Vetor contendo o \code{SERCODIGOTROLL}
+#' da(s) serie(s) requisitada(s).
 #'
-#' @author Luiz Eduardo Gomes, \email{luiz.gomes@@ipea.gov.br} ou \email{gomes.leduardo@@gmail.com}.
+#' @author Luiz Eduardo Gomes, \email{luiz.gomes@@ipea.gov.br}
+#' ou \email{gomes.leduardo@@gmail.com}.
 #'
 #' @examples
 #' # ------ Multiplas series, exibindo grafico
-#' gen_verif <- genericaVerif(serie = c("GM12_DOW12","ABATE12_ABPENO12","MTE12_SALMIN12"))
+#' gen_verif <- generica(serie = c("GM12_DOW12", "ABATE12_ABPENO12",
+#' "MTE12_SALMIN12"))
 #'
 #' @export
 
-genericaVerif <- function(serie)
-{
+generica <- function(serie) {
+
   # ------ Desligando notacao cientifica
-  options(scipen=999)
+  options(scipen = 999)
 
   # ------ Encontrando nomes
-  nomes <- encontraSerie(serie = serie, plotar = FALSE)
+  nomes <- EncontraSerie(serie = serie, plotar = FALSE)
   nomes <- nomes$SERCODIGOTROLL
 
   # CARREGANDO METADADOS ----------------------------------------
 
   # ------ Abrindo conexao
-  con <-  RODBC::odbcConnect("ipeadata",uid="",pwd="")
+  con <-  RODBC::odbcConnect(dsn = "ipeadata", uid = "", pwd = "")
 
   # ------ Consulta SQL
   serie <- RODBC::sqlQuery(con,
@@ -44,7 +47,7 @@ genericaVerif <- function(serie)
                                   "CAST (ipea.vw_Valor.VALDATA as NUMERIC) as VALDATA, ",
                                   "ipea.vw_Valor.VALVALOR FROM ipea.vw_Valor ",
                                   "WHERE ipea.vw_Valor.SERCODIGO IN (",
-                                  paste0("'",nomes,"'", collapse = ", "),") ",
+                                  paste0("'", nomes, "'", collapse = ", "),") ",
                                   "and ipea.vw_Valor.VALVALOR IS NOT NULL order by VALDATA;"))
 
   # ------ Fechando conexao
@@ -54,11 +57,11 @@ genericaVerif <- function(serie)
 
   # ------ Planilha Generica
   GENERICAv <- data.frame(VALDATA = unique(serie$VALDATA))
-  for (i in 1:length(nomes))
-  {
-    GENERICAv <- merge(x = GENERICAv,
-                       y = subset(x = serie,subset = serie$SERCODIGO == nomes[i],
-                                  select = c("VALDATA","VALVALOR")),
+  for (i in 1:length(nomes)) {
+    GENERICAv <- merge(GENERICAv,
+                       subset(x = serie,
+                              subset = serie$SERCODIGO == nomes[i],
+                              select = c("VALDATA", "VALVALOR")),
                        by = "VALDATA",
                        all = TRUE)
     names(GENERICAv)[i+1] <- nomes[i]
@@ -68,9 +71,11 @@ genericaVerif <- function(serie)
   GENERICAv$VALDATA <- as.Date(GENERICAv$VALDATA, origin = "1900-01-01")
 
   # ------ Removendo possivel linha de NA
-  GENERICAind <- data.frame(ind = is.na(GENERICAv[,-1]))
-  erros <- sum(rowSums(GENERICAind)==ncol(GENERICAind))
-  if(erros>0){GENERICAv <- GENERICAv[-which(rowSums(GENERICAind)==ncol(GENERICAind)),]}
+  GENERICAind <- data.frame(ind = is.na(GENERICAv[, -1]))
+  erros <- sum(rowSums(GENERICAind) == ncol(GENERICAind))
+  if (erros > 0) {
+    GENERICAv <- GENERICAv[-which(rowSums(GENERICAind) == ncol(GENERICAind)), ]
+  }
 
   # ------ Resultado
   return(GENERICAv)
